429eb99b9ae86d2a42c01d4dc40f7bbe76610eb8
Ryan Kopf <git@ryankopf.com>
2025-09-11 20:51:43 -0500

    X

--- next.sh
+++ next.sh
@@ -3,6 +3,7 @@ if [ "$(id -u)" -ne 0 ]; then
   echo "Please run as root"
   exit
 fi
+chmod +x next.sh build.sh
 service ryancloud stop
 cp ./target/release/ryancloud /usr/local/bin/ryancloud
 chmod 755 /usr/local/bin/ryancloud
