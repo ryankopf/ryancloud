ed29d948381fd567690225ed0d221dc5aa259162
Ryan Kopf <git@ryankopf.com>
2025-09-11 20:53:10 -0500

    X

--- next.sh
+++ next.sh
@@ -7,4 +7,6 @@ chmod +x next.sh build.sh
 service ryancloud stop
 cp ./target/release/ryancloud /usr/local/bin/ryancloud
 chmod 755 /usr/local/bin/ryancloud
+# Grant capability to bind to port 80 without root
+setcap 'cap_net_bind_service=+ep' /usr/local/bin/ryancloud
 service ryancloud restart
