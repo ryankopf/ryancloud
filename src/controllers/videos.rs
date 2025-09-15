787b0d533fd4d3092572c1d1db9035d535816c70
Ryan Kopf <github@ryankopf.com>
2025-09-15 04:01:27 -0500

    X

--- src/controllers/videos.rs
+++ src/controllers/videos.rs
@@ -10,7 +10,7 @@ pub async fn show(
     let view_name = "show";
     let form_path = format!("./src/views/{}/{}.html", controller, view_name);
     let content = std::fs::read_to_string(&form_path).unwrap_or_default();
-    let filename = video_filename.into_inner();
+    let filename = format!("/{}", video_filename.into_inner());
     let html = content.replace("{{filename}}", &filename);
     println!("Trying to serve video: {}", filename);
     HttpResponse::Ok().content_type("text/html").body(html)
