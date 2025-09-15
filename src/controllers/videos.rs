428291b1603ee9df0188cfe1bc6ab3fca1b135e2
Ryan Kopf <github@ryankopf.com>
2025-09-15 04:12:12 -0500

    X

--- src/controllers/videos.rs
+++ src/controllers/videos.rs
@@ -1,17 +1,18 @@
-use actix_web::{get, HttpResponse, web};
+use actix_web::{get, web, HttpResponse};
+use std::path::PathBuf;
 
-// #[mvc_views]
-#[get("/videos/{video_filename}")]
-pub async fn show(
-    video_filename: web::Path<String>,
-) -> HttpResponse {
+#[get("/videos/{video_path:.*}")]
+pub async fn show(video_path: web::Path<PathBuf>) -> HttpResponse {
     let controller = "videos";
     let view_name = "show";
     let form_path = format!("./src/views/{}/{}.html", controller, view_name);
     let content = std::fs::read_to_string(&form_path).unwrap_or_default();
-    let filename = format!("/{}", video_filename);
+
+    let filename = format!("/{}", video_path.display());
     let html = content.replace("{{filename}}", &filename);
+
     println!("Trying to serve video: {}", filename);
+
     HttpResponse::Ok().content_type("text/html").body(html)
 }
 
