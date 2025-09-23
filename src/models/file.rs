398250a9d995eaf80cd6562f9d936d93e33af52e
Ryan Kopf <github@ryankopf.com>
2025-09-23 02:00:53 -0500

    X

--- src/models/file.rs
+++ src/models/file.rs
@@ -4,7 +4,7 @@ impl File {
     pub fn video_preview(subpath: &str, video: &str) -> String {
         format!(
             "<a href='/videos/{link}' style='max-width:250px;display:inline-block;'>\
-            <img src='{subpath}/thumbs/{video}.webp' class='img-fluid rounded border' alt='{video}' style='width:100%;'>\
+            <img src='/{subpath}/thumbs/{video}.webp' class='img-fluid rounded border' alt='{video}' style='width:100%;'>\
             <div class='text-center text-white position-absolute mx-auto px-2' style='margin-top:-30px;'>{filename}</div></a>",
             link = if subpath.is_empty() { format!("/{}", video) } else { format!("/{}/{}", subpath, video) },
             subpath = subpath,
