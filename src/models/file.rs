2433c1865dde5d45dd1596a4d0b9b5c70c6cf802
Ryan Kopf <github@ryankopf.com>
2025-09-23 02:05:22 -0500

    X

--- src/models/file.rs
+++ src/models/file.rs
@@ -3,9 +3,9 @@ pub struct File;
 impl File {
     pub fn video_preview(subpath: &str, video: &str) -> String {
         format!(
-            "<a href='/videos/{link}' style='max-width:250px;display:inline-block;'>\
+            "<a href='/videos/{link}' style='max-width:250px;display:inline-block;' class='video_preview'>\
             <img src='/{subpath}/thumbs/{video}.webp' class='img-fluid rounded border' alt='{video}' style='width:100%;'>\
-            <div class='text-center text-white position-absolute mx-auto px-2' style='margin-top:-30px;'>{filename}</div></a>",
+            <div class='text-center text-white position-absolute mx-auto px-2 filename'>{filename}</div></a>",
             link = if subpath.is_empty() { format!("/{}", video) } else { format!("/{}/{}", subpath, video) },
             subpath = subpath,
             video = video,
@@ -44,9 +44,9 @@ impl File {
 
     pub fn clip_video_preview(clip: &crate::models::clip::Model) -> String {
         format!(
-            "<a href='/segments/{clip_filename}' style='max-width:250px;display:inline-block;'>\
+            "<a href='/segments/{clip_filename}' style='max-width:250px;display:inline-block;' class='video_preview'>\
             <img src='/segments/thumbs/{clip_filename}.webp' class='img-fluid rounded border' alt='{clip_filename}' style='width:100%;'>\
-            <div class='text-center text-white position-absolute mx-auto px-2' style='margin-top:-30px;'>{clip_filename} ({start}-{end})</div></a>",
+            <div class='text-center text-white position-absolute mx-auto px-2 filename'>{clip_filename} ({start}-{end})</div></a>",
             clip_filename = clip.clip_filename,
             start = clip.start,
             end = clip.end,
